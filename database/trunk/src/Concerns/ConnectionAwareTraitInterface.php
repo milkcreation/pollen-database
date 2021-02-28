520d5f908549ee8eac2dd975c8a80324dcda17f0
5upp0r7 <5upp0r7@23fad6b8-6d8e-48a5-9e8d-e4f1c0242fc1>
2021-02-28 18:57:04 +0000

    database >> master

---
+++ database/trunk/src/Concerns/ConnectionAwareTraitInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pollen\Database\Concerns;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+
+/**
+ * @mixin Model
+ * @mixin Builder
+ */
+interface ConnectionAwareTraitInterface
+{
+    /**
+     * Récupération du préfixe des tables.
+     *
+     * @return string
+     */
+    public function getTablePrefix(): string;
+}
